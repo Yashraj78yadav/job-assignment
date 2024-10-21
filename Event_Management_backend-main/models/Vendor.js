3fbfb2a5a2bdd125872521893cf3398559b8105b
Yashraj78yadav <richyashrajyadav@gmail.com>
2024-10-21 17:36:29 +0530

    updates

--- Event_Management_backend-main/models/Vendor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const VendorSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }],
-});
-
-module.exports = mongoose.model('Vendor', VendorSchema);
--- Event_Management_backend-main/models/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    // Dummy role check logic for demo purposes
-    const role = req.headers['role'];
-    if (!role || !['admin', 'vendor', 'user'].includes(role)) {
-      return res.status(403).json({ message: 'Access forbidden' });
-    }
-    next();
-  };
-  
